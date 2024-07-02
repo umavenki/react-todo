da6b91b69867f5d2d2662abc8252faa2382bd6e3
Uma Venki <iamumamsekar@gmail.com>
2024-07-02 13:47:22 -0700

    Homework Completed

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,11 +1,21 @@
-function AddTodoForm() {
+function AddTodoForm(props) {
+  function handleAddTodo(event) {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(event.target.title);
+    const todoTitle = event.target.title.value;
+    console.log(todoTitle);
+    props.onAddTodo(todoTitle);
+    event.target.reset();
+  }
+
   return (
     <div>
       <h1> AddTodoForm</h1>
-      <form>
+      <form onSubmit={handleAddTodo}>
         <label>
           Title
-          <input id="todoTitle" />
+          <input id="todoTitle" name="title" />
         </label>
         <button>Add</button>
       </form>
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import { useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+
   return (
     <div>
       <h1>TodoList</h1>
       <TodoList></TodoList>
-      <AddTodoForm></AddTodoForm>
+      <AddTodoForm onAddTodo={setNewTodo}></AddTodoForm>
+      <p>{newTodo}</p>
     </div>
   );
 }
