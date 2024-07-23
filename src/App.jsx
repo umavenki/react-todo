b8212c4d82882e704bd4c3880220fa9f29706e2e
Uma Venki <iamumamsekar@gmail.com>
2024-07-22 20:07:02 -0700

    Local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+}
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   function addTodo(newTodo) {
-    setTodoList((prevTododList) => [...prevTododList, newTodo]);
+    setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
   }
 
   return (
